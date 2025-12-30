60f6cc066c84162963a9ea2fb3d069aa88208050
AlinaLukyanova25 <lukyanova-alina2020@yandex.ru>
2025-12-30 06:14:55 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -22,7 +22,7 @@ export function toModal(card, id) {
         accept="image/*"
         style="display: none;">
     <label class="modal__label" for="modal-date-production">Дата изготовления</label>
-    <input class="input-field modal__date-production" id="modal-date-production" type="date" name="dateProd" data-id="${id}" max="${dateStartString}" value="${card.productionDate}" required>
+    <input class="input-field modal__date-production" id="modal-date-production" type="date" name="dateProd" data-id="${id}" max="${dateStartString}" value="${card.productionDate}" style="width: 100%;" required>
     <div class="modal__auto-calc">
     <label class="modal__label" for="auto">Автоматический расчет по</label>
     <select class="input-field modal__select" id="auto" style="width: 100%;">
@@ -30,11 +30,11 @@ export function toModal(card, id) {
     <option value="day">дням</option>
     <option value="month">месяцам</option>
     </select>
-    <button class="modal__calculator"><img class="modal__img" src="../img/calculator.svg" alt="Калькулятор"></button>
+    <button class="modal__calculator"><img class="modal__img" src="img/calculator.svg" alt="Калькулятор"></button>
     </div>
     <button class="btn modal__button--calc">Автоматический расчет</button>
     <label class="modal__label" for="modal-date">Дата окончания срока</label>
-    <input class="input-field modal__end-date" id="modal-date" type="date" name="date" data-id="${id}" min="${nextDayString}" value="${card.expiryDate}" required>
+    <input class="input-field modal__end-date" id="modal-date" type="date" name="date" data-id="${id}" min="${nextDayString}" value="${card.expiryDate}" style="width: 100%;" required>
     <button class="btn modal__button" type="submit">Сохранить изменения</button>
     <button class="btn modal__push-archive">Добавить в архив</button>
     `
@@ -88,7 +88,7 @@ function createModalReturn(product, id) {
         accept="image/*"
         style="display: none;">
     <label class="modal-return__label" for="modal-return-date-prod">Дата изготовления</label>
-    <input class="input-field modal-return__prod-date" id="modal-return-date-prod" type="date" name="dateProd" data-modal="${id}" max="${dateStartString}" required>
+    <input class="input-field modal-return__prod-date" id="modal-return-date-prod" type="date" name="dateProd" data-modal="${id}" max="${dateStartString}" style="width: 100%;" required>
     <div class="modal-return__auto-calc">
     <label class="modal-return__label" for="return-auto">Автоматический расчет по</label>
     <select class="input-field modal-return__select" id="return-auto" style="width: 100%;">
@@ -96,11 +96,12 @@ function createModalReturn(product, id) {
     <option value="day">дням</option>
     <option value="month">месяцам</option>
     </select>
-    <button class="modal__calculator"><img class="modal__img" src="../img/calculator.svg" alt="Калькулятор"></button>
+    <button class="modal__calculator"><img class="modal__img" src="img/calculator.svg" alt="Калькулятор"></button>
     </div>
     <button class="btn modal-return__button--calc">Автоматический расчет</button>
     <label class="modal-return__label" for="modal-return-date">Дата окончания срока</label>
-    <input class="input-field modal-return__end-date" id="modal-return-date" type="date" name="date" data-modal="${id}" min="${nextDayString}" required>
+    <input class="input-field modal-return__end-date" id="modal-return-date" type="date" name="date" data-modal="${id}" min="${nextDayString}" style="width: 100%;" required>
     <button class="btn modal-return__button" type="submit">Вернуть из архива</button>
     `
+
 }
